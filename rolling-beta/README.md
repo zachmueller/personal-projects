0978d019dc8b95499e9fc35dcaf9ecf41f1ffa7a
Zach Mueller <zacharymueller@gmail.com>
2014-07-07 21:22:49 -0500

    Core Functionality
    Start building ad hoc R scripts that will later drive the core
    functionality of the final Shiny app

---
+++ rolling-beta/README.md
@@ -0,0 +1,12 @@
+# Rolling Beta Chart
+
+## Overview
+- Based on original idea from Ron Sweet, professor at UTSA
+- Quickly visualize the change in a stock's Beta over time to better estimate what its Beta will be going forward
+- Beta calculated on a rolling 3 year basis (to be adjustable in the future)
+
+## Progression
+1. Originally performed manually in Excel (as taught by Ron Sweet)
+2. Automated in Excel via VBA
+3. Further automated in the browser via PHP and javascript (currently living on [my cloud server] (http://www.zachmueller.com/apps/charts))
+4. Rebuild and expand browser app using R and Shiny (work in progress)
--- rolling-beta/ad hoc.R
+++ rolling-beta/ad hoc.R
@@ -1,8 +1,53 @@
 # load libraries
 library(quantmod)
 
-# set date range
+# set date
 start.date <- as.Date("1960-01-01")
 
+# create list to download
+sym <- c("AMZN","AAPL","^GSPC")
+
 # download data into time series
-getSymbols(c("AMZN","AAPL"), src = "yahoo", from = start.date, auto.assign = FALSE)
+sym <- getSymbols(sym, src = "yahoo", from = start.date, auto.assign = TRUE)
+
+
+# loop through each returned symbol
+min.start.date <- start.date
+for (i in sym) {
+	# capture each stock's earliest date index
+	#   and store the greatest of that and previous min.start.date
+	min.start.date <- max(index(get(i))[1]
+		,min.start.date)
+}
+
+GSPC <- GSPC.bk
+AMZN <- AMZN.bk
+AAPL <- AAPL.bk
+
+# trim each time series to the latest start date
+max.date <- max(index(get(sym[1])))
+GSPC <- window(GSPC, start = min.start.date
+	,end = max.date)
+
+
+# calculate percent change of daily adjusted prices
+d <- AAPL[,6]
+percent.change <- d / lag(d, 1) - 1
+
+
+
+# create population functions
+# standard deviation (population)
+sd.pop <- function(x) {
+	sqrt(sum((x-mean(x))^2) / length(x))
+}
+
+# covariance (population)
+cov.pop <- function(x, y) {
+	sum((x-mean(x))*(y-mean(y)))/ length(x)
+}
+
+# create function to calculate the beta of a stock
+beta <- function(stock, market) {}
+
+# working with zoo objects: http://cran.r-project.org/web/packages/zoo/vignettes/zoo-quickref.pdf
